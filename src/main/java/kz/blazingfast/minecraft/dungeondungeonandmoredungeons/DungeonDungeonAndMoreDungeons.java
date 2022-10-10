9744a855c2a66b8395a659cb4e247ffd45a9d05c
serge <aboba@ubuntu.ubuntu-domain>
2022-10-10 15:03:25 +0600

    init project

---
+++ src/main/java/kz/blazingfast/minecraft/dungeondungeonandmoredungeons/DungeonDungeonAndMoreDungeons.java
@@ -0,0 +1,17 @@
+package kz.blazingfast.minecraft.dungeondungeonandmoredungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DungeonDungeonAndMoreDungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
