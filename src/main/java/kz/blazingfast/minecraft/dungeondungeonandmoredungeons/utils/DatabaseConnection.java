fcc7573fac77183b67415724dab8a15a28adfbc3
BuildTools <unconfigured@null.spigotmc.org>
2022-10-13 14:32:08 +0600

    Creating class DatabaseConnection.java using Singleton Pattern

---
+++ src/main/java/kz/blazingfast/minecraft/dungeondungeonandmoredungeons/utils/DatabaseConnection.java
@@ -0,0 +1,34 @@
+package kz.blazingfast.minecraft.dungeondungeonandmoredungeons.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private DatabaseConnection(String url, String username, String password) throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DatabaseConnection getInstance(String url, String username, String password) throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection(url,username,password);
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection(url, username, password);
+        }
+
+        return instance;
+    }
+}
